f16c278f4eddd1dbe03aa36ca9b1bb3618ae141f
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2021-10-28 20:41:16 -0600

    Create a version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement()
+{
+	versionCatalogs()
+	{
+		val deps by creating()
+		{
+			from(files("versions.toml"))
+		}
+	}
+}
+
 rootProject.name = "shorthand"
